5761c3482f88a03e496316b1a5b26207c1e87ef0
Ali Murtaza <107575676+AliMurtazaHaral@users.noreply.github.com>
2023-05-26 19:15:16 +0500

    ui revisions

---
+++ lib/widgits/custom_text_widgit.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomText extends StatelessWidget {
+  final title;
+  final fontWeight;
+  final textColor;
+  final fontSize;
+  final textPosition;
+  final textheight;
+
+  CustomText({
+    this.title,
+    this.fontWeight,
+    this.textColor,
+    this.fontSize,
+    this.textPosition,
+    this.textheight,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Expanded(
+        child: Text(
+          title,
+          textAlign: textPosition,
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          style: GoogleFonts.poppins(
+            fontSize: fontSize,
+            fontWeight: fontWeight,
+            color: textColor,
+            height: textheight,
+          ),
+        ),
+      ),
+    );
+  }
+}
